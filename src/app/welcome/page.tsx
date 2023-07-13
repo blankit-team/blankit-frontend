73085d42e20caf7840fc6161ccd78aeb1e52f670
oaoong <pizzachicken@kakao.com>
2023-07-13 17:13:29 +0900

    :tada: welcome 페이지 생성

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import WelcomeUser from './components/welcome-user'
+
+export default function WelcomePage() {
+  return (
+    <div className="flex flex-col items-center gap-4 pt-32">
+      <WelcomeUser />
+    </div>
+  )
+}
